92a136f72608e467663a303506f817aa30bd6669
takashi <tks.srtr89@gmail.com>
2026-02-05 19:01:28 +0900

    add: ui test

--- internal/ui/log.go
+++ internal/ui/log.go
@@ -11,14 +11,16 @@ const (
 	colorGreen  = "\033[32m"
 	colorYellow = "\033[33m"
 	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
 )
 
 func Info(w io.Writer, msg string, args ...any) {
-	fmt.Fprintf(w, "\033[34m[Info] "+msg+"\033[0m\n", args...)
+	fmt.Fprintln(w, colorPurple+"[Info] "+fmt.Sprintf(msg, args...)+colorReset)
 }
 
 func Success(w io.Writer, msg string, args ...any) {
-	fmt.Fprintln(w, colorGreen+"[Success] "+fmt.Sprintf(msg, args...)+colorReset)
+	fmt.Fprintln(w, colorCyan+"[Success] "+fmt.Sprintf(msg, args...)+colorReset)
 }
 
 func Warn(w io.Writer, msg string, args ...any) {
